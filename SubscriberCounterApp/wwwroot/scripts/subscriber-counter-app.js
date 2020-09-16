3731f7d62533f1583123ed6ae7f1ab96834dd5ea
RedPlus <redplus@redplus.net>
2020-09-16 00:00:39 +0000

    SubscriberCounter

--- SubscriberCounterApp/wwwroot/scripts/subscriber-counter-app.js
+++ SubscriberCounterApp/wwwroot/scripts/subscriber-counter-app.js
@@ -1,2 +1,52 @@
-console.log("구독자 수 관리 앱");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SubscriberCounter = exports.HtmlResponse = void 0;
+//console.log("구독자 수 관리 앱");
+//[1] HTML 페이지의 특정 div 태그에 HTML 출력
+var HtmlResponse = /** @class */ (function () {
+    function HtmlResponse() {
+        // Empty
+    }
+    HtmlResponse.write = function (html) {
+        HtmlResponse.divHtml.innerHTML = html;
+    };
+    return HtmlResponse;
+}());
+exports.HtmlResponse = HtmlResponse;
+//[2] 구독자 수 관리에 필요한 기능
+var SubscriberCounter = /** @class */ (function () {
+    // 생성자
+    function SubscriberCounter(title) {
+        this.title = title;
+        // 필드
+        this._count = 0;
+    }
+    Object.defineProperty(SubscriberCounter.prototype, "count", {
+        // 속성
+        get: function () {
+            return this._count;
+        },
+        set: function (value) {
+            this._count = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // 메서드
+    SubscriberCounter.prototype.increment = function (cnt) {
+        this._count += cnt;
+    };
+    SubscriberCounter.prototype.decrement = function (cnt) {
+        this._count -= cnt;
+    };
+    return SubscriberCounter;
+}());
+exports.SubscriberCounter = SubscriberCounter;
+//[!] 실행
+var Root = /** @class */ (function () {
+    function Root() {
+    }
+    return Root;
+}());
+var root = new Root();
 //# sourceMappingURL=subscriber-counter-app.js.map
--- SubscriberCounterApp/wwwroot/scripts/subscriber-counter-app.ts
+++ SubscriberCounterApp/wwwroot/scripts/subscriber-counter-app.ts
@@ -1,1 +1,38 @@
-﻿console.log("구독자 수 관리 앱");
+﻿//console.log("구독자 수 관리 앱");
+//[1] HTML 페이지의 특정 div 태그에 HTML 출력
+export class HtmlResponse {
+    private constructor() {
+        // Empty
+    }
+    static divHtml: HTMLDivElement;
+    static write(html: string) {
+        HtmlResponse.divHtml.innerHTML = html; 
+    }
+}
+//[2] 구독자 수 관리에 필요한 기능
+export class SubscriberCounter {
+    // 필드
+    private _count = 0; 
+    // 생성자
+    constructor(public title: string) {
+    }
+    // 속성
+    get count() {
+        return this._count; 
+    }
+    set count(value: number) {
+        this._count = value; 
+    }
+    // 메서드
+    increment(cnt: number) {
+        this._count += cnt; 
+    }
+    decrement(cnt: number) {
+        this._count -= cnt; 
+    }
+}
+//[!] 실행
+class Root {
+
+}
+const root = new Root();
